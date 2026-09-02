ae70719a2558d2a74f0226e91895419574da82fd
Leif Davisson <leifdavisson@example.com>
2026-09-01 20:42:22 -0700

    chore: remove patch scripts

--- scratch.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('server/data/cmp.db')
-c = conn.cursor()
-c.execute("SELECT sensor_id, hostname, mac_address FROM sensors")
-for row in c.fetchall():
-    print(row)
-conn.close()
